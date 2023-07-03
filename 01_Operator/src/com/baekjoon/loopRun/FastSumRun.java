35326f4b11f2e54271d67b521c079fdf1923f682
YoonTarget <igeek437@gmail.com>
2023-07-03 21:29:57 +0900

    7/3

---
+++ 01_Operator/src/com/baekjoon/loopRun/FastSumRun.java
@@ -0,0 +1,35 @@
+package com.baekjoon.loopRun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FastSumRun {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int num = br.read();
+		
+		if(num >= 1 && num <= 1000000) {
+			
+			for(int i = 0; i < num; i++) {
+				
+				int a = br.read();
+				int b = br.read();
+				
+				if(a >= 1 && a <= 1000 && b >= 1 && b <= 1000) {
+					
+					
+					
+				}
+				
+			}
+			
+		}
+		
+		
+	}
+
+}
--- 01_Operator/src/com/baekjoon/loopRun/Sum2Run.java
+++ 01_Operator/src/com/baekjoon/loopRun/Sum2Run.java
@@ -2,19 +2,19 @@ package com.baekjoon.loopRun;
 
 import java.util.Scanner;
 
-public class SumRun {
+public class Sum2Run {
 
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
 		int num = sc.nextInt();
+		int total = 0;
 		
-		for(int i = 0; i < num; i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			System.out.println(a + b);
+		for(int i = 1; i <= num; i++) {
+			total += i;
 		}
+		System.out.println(total);
 		
 		sc.close();
 
